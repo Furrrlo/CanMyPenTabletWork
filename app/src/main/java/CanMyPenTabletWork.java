f9fa1fc18075dd31425fa5f9ff153328fd4ad72f
Furrrlo <ferloisbad@gmail.com>
2021-11-19 22:02:31 +0100

    Add (almost) fully working hooks
    Only misses actually executing the provided script.
    Before that, I need to serialize scripts to disk to then be able to run those.

--- app/src/main/java/CanMyPenTabletWork.java
+++ app/src/main/java/CanMyPenTabletWork.java
@@ -1,7 +1,12 @@
-import me.ferlo.cmptw.hook.KeyboardHookService;
+import me.ferlo.cmptw.hook.*;
+import me.ferlo.cmptw.process.Process;
+import me.ferlo.cmptw.process.ProcessService;
+import me.ferlo.cmptw.script.ScriptEngine;
+import me.ferlo.cmptw.script.ScriptEnvironment;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
-import java.awt.event.KeyEvent;
+import java.awt.*;
+import java.util.Optional;
 
 public class CanMyPenTabletWork {
     public static void main(String[] args) throws Exception {
@@ -9,25 +14,60 @@ public class CanMyPenTabletWork {
         SLF4JBridgeHandler.removeHandlersForRootLogger();
         SLF4JBridgeHandler.install();
 
-        final KeyboardHookService service = KeyboardHookService.create();
-        service.addListener(event -> {
-            if(!event.isKeyDown())
-                return false;
+        final var keyboardHookService = KeyboardHookService.create();
+        final HookService hookService = new InMemoryHookService();
+        final var processService = ProcessService.create();
+        final var scriptEngine = ScriptEnvironment.create().discoverEngine().get();
 
-            final StringBuilder sb = new StringBuilder();
-            sb.append(event.device().getId());
-            sb.append(": ");
-            if(event.modifiers() != 0)
-                sb.append(event.getModifiersText(" + ")).append(" + ");
-            sb.append(KeyEvent.getKeyText(event.awtKeyCode()));
-            System.out.println(sb);
-            return true;
-        });
-        service.register();
+        keyboardHookService.addListener(event -> hook(hookService, processService, scriptEngine, event));
+        keyboardHookService.register();
+    }
 
-        System.out.println("Started");
-        while(true) {
-            Thread.sleep(100000);
-        }
+    private static boolean hook(HookService hookService,
+                                ProcessService processService,
+                                ScriptEngine scriptEngine,
+                                KeyboardHookEvent event) {
+        if(!event.isKeyDown())
+            return false;
+
+        final Optional<Hook> maybeHook = hookService.getSaved().stream()
+                .filter(h -> h.device().id().equalsIgnoreCase(event.device().getId()))
+                .findFirst();
+        if(maybeHook.isEmpty())
+            return false;
+
+        final Hook hook = maybeHook.get();
+        final Optional<Process> maybeProcess = processService.getProcessForPid(event.pid());
+        if(maybeProcess.isEmpty())
+            return fallbackBehavior(hook);
+
+        final Process process = maybeProcess.get();
+        final Optional<Hook.ApplicationHook> maybeApplication = hook.applicationHooks().stream()
+                .filter(a -> a.application().process().equalsIgnoreCase(process.name()))
+                .findFirst();
+        if(maybeApplication.isEmpty())
+            return fallbackBehavior(hook);
+
+        final Hook.ApplicationHook application = maybeApplication.get();
+        final Optional<Hook.HookScript> maybeScript = application.scripts().stream()
+                .filter(s -> s.keyStroke().keyCode() == event.awtKeyCode() && s.keyStroke().modifiers() == event.modifiers())
+                .findFirst();
+        if(maybeScript.isEmpty())
+            return fallbackBehavior(hook);
+
+        final Hook.HookScript script = maybeScript.get();
+        scriptEngine.execute("msgbox % \"my ahk version: \" A_AhkVersion"); // TODO
+        return true;
+    }
+
+    private static boolean fallbackBehavior(Hook hook) {
+        return switch (hook.fallbackBehavior()) {
+            case IGNORE -> false;
+            case DELETE -> true;
+            case DELETE_AND_PLAY_SOUND -> {
+                Toolkit.getDefaultToolkit().beep();
+                yield true;
+            }
+        };
     }
 }
