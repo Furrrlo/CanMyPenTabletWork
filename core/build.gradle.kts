833ada686d7658a5f3e265c63138001a84812641
Furrrlo <ferloo@protonmail.com>
2021-12-09 11:30:14 +0100

    Fix :core declaring dependencies as implementation

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 
 dependencies {
     // TODO: I don't like these dependency, but I can't think  of any way of removing them
-    implementation(libs.rsyntaxtextarea)
-    implementation(libs.autocomplete)
+    api(libs.rsyntaxtextarea)
+    api(libs.autocomplete)
 }
